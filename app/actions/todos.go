d4b370645f97030060be984bdc6b5a30af260d17
Edwin Chiu <echiu@Motus-C02GJ3PPMD6R.local>
2021-12-03 10:09:02 -0500

    [ECHIU2] query database information into index page

--- app/actions/todos.go
+++ app/actions/todos.go
@@ -2,10 +2,21 @@ package actions
 
 import (
 	"net/http"
+	"todolist/app/models"
 
 	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/pop/v5"
 )
 
 func Index(c buffalo.Context) error {
+	tx := c.Value("tx").(*pop.Connection)
+	todos := models.Todos{}
+
+	if err := tx.All(&todos); err != nil {
+		return c.Error(http.StatusInternalServerError, err)
+	}
+
+	c.Set("todos", todos)
+
 	return c.Render(http.StatusOK, r.HTML("/todos/index.plush.html"))
 }
--- app/models/todo.go
+++ app/models/todo.go
@@ -5,10 +5,12 @@ import (
 )
 
 type Todo struct {
-	Title     string    `db:"title" json:"title"`
-	Details   string    `db:"details" json:"details"`
-	CreatedAt time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
+	LimitDate   time.Time `db:"limit_date" json:"limit_date"`
+	IsCompleted bool      `db:"is_completed" json:"is_completed"`
+	Title       string    `db:"title" json:"title"`
+	Details     string    `db:"details" json:"details"`
+	CreatedAt   time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt   time.Time `db:"updated_at" json:"updated_at"`
 }
 
 type Todos []Todo
